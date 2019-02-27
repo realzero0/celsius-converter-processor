27311936354987d7f74a1a63cdb57cf35b3858e7
Song, Jinyoung <jinyoung.song@rakuten.com>
2019-02-27 09:14:48 +0900

    change annotationˆ

--- src/main/java/demo/celsius/converter/celsiusconverterprocessor/CelsiusConverterProcessorConfiguration.java
+++ src/main/java/demo/celsius/converter/celsiusconverterprocessor/CelsiusConverterProcessorConfiguration.java
@@ -1,16 +1,16 @@
 package demo.celsius.converter.celsiusconverterprocessor;
 
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 
 @EnableBinding(Processor.class)
 public class CelsiusConverterProcessorConfiguration {
 
     @StreamListener(Processor.INPUT)
-    @Output(Processor.OUTPUT)
+    @SendTo(Processor.OUTPUT)
     public String convertToCelsius(@Payload String data) {
         System.out.println(data);
         return data;
